ac19733683a22891cfc9a24b1563547107ca2147
ImMano0 <UrMa1371@gmail.com>
2023-03-16 19:27:55 +0500

    misol

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+a=69
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer=96
 #Print the value of the 'answer'
-
-
+print(answer)
